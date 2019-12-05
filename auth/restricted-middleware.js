8f8f308b0cf58aaa8764752d006dfa631c7a9387
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-04 21:45:45 -0600

    added if statement to check username and password exist in restricted-middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,15 +3,19 @@ const Users = require("../data/dbHelper");
 
 module.exports = async function(req, res, next) {
   const { username, password } = req.headers;
-  return await Users.getUser(username)
-    .then(user => {
-      if (user.length && bcrypt.compareSync(password, user[0].password)) {
-        next();
-      } else {
-        res.status(400).json({ message: "invalid credentials" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json(err);
-    });
+  if (username && password) {
+    return await Users.getUser(username)
+      .then(user => {
+        if (user.length && bcrypt.compareSync(password, user[0].password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch(err => {
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(400).json({ message: "please input valid credentials" });
+  }
 };
