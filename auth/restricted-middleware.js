4e6d8accfcb1bb64d5c2889a74cfc005d07dfa5c
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-06 12:42:48 -0600

    changed restricted middleware to check for session and then next() if session and user prop for session exist

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,20 +1,6 @@
-const bcrypt = require("bcryptjs");
-const Users = require("../data/dbHelper");
-
 module.exports = async function(req, res, next) {
-  const { username, password } = req.headers;
-  if (username && password) {
-    return await Users.getUser(username)
-      .then(user => {
-        if (user.length && bcrypt.compareSync(password, user[0].password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "invalid credentials" });
-        }
-      })
-      .catch(err => {
-        res.status(500).json(err);
-      });
+  if (req.session && req.session.user) {
+    next();
   } else {
     res.status(400).json({ message: "please input valid credentials" });
   }
