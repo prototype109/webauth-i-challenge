0bd82351438cfb01a2130774023fdc303283909e
Brandon Nichols <brandonaronnichols@gamil.com>
2019-12-06 12:38:19 -0600

    added connect-session-knex and express-session to dependancies and created a sessionConfig object where the server is using it

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const session = require("express-session");
+const knexSession = require("connect-session-knex");
 const server = express();
 const userRoute = require("./routes/userRoute");
 
+const sessionConfig = {
+  name: "session",
+  secret: "secretKey",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new knexSession({
+    knex: require("./data/dbConfig"),
+    tablename: "current-session",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
 server.use("/api", userRoute);
 
 module.exports = server;
